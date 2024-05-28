96f3aab560ce012cf2ab01795f12a0a17a0f5047
Mahmoud <mahmoud@mahmoudibrahim.me>
2024-05-28 17:41:59 +0400

    set active links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilm, faTv } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item">
+        <li
+          className={`navbar-item ${
+            location.pathname === "/tv" ? "active" : ""
+          }`}
+        >
           <a href="/tv" className="navbar-link">
             مسلسلات <FontAwesomeIcon icon={faTv} />
           </a>
         </li>
-        <li className="navbar-item">
+        <li
+          className={`navbar-item ${location.pathname === "/" ? "active" : ""}`}
+        >
           <a href="/" className="navbar-link">
             أفلام <FontAwesomeIcon icon={faFilm} />
           </a>
